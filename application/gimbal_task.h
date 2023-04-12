98f435ce54a19be6993977b6d1c12b038d8dc50f
Cjsah <2684117397@qq.com>
2023-04-12 17:45:10 +0800

    update

--- application/gimbal_task.h
+++ application/gimbal_task.h
@@ -95,10 +95,10 @@
 
 
 #define YAW_RC_SEN    -0.000005f
-#define PITCH_RC_SEN  -0.000006f //0.005
+#define PITCH_RC_SEN  -0.000001f //0.005
 
 #define YAW_MOUSE_SEN   0.00005f
-#define PITCH_MOUSE_SEN 0.00015f
+#define PITCH_MOUSE_SEN 0.000015f
 
 #define YAW_ENCODE_SEN    0.01f
 #define PITCH_ENCODE_SEN  0.01f
